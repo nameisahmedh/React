a9f7c11dd07720d95b5adbe03ab82b4cb3130257
nameisahmedh <mdqamarahmed123@gmail.com>
2025-08-26 19:25:43 +0530

    basic 1

---
+++ 01_project/src/App.jsx
@@ -0,0 +1,16 @@
+import Coffee from "./coffee"
+
+function App() {
+ 
+
+  return (
+
+      <>
+        <h1>Heyyy</h1>
+        <Coffee/>
+      </>
+  
+  )
+}
+
+export default App
---
+++ 01_project/src/coffee.jsx
@@ -0,0 +1,7 @@
+// When you are creating a component it should be capitilise
+function Coffee(){
+    return(
+        <h1>Coffee is ready</h1>
+    )
+}
+export default Coffee
