af51c3fc4c34126850cfa170517405cbd38f88b6
nameisahmedh <mdqamarahmed123@gmail.com>
2025-08-26 21:22:31 +0530

    reactElement Internal creation

--- 01_project/src/App.jsx
+++ 01_project/src/App.jsx
@@ -2,11 +2,12 @@ import Coffee from "./coffee"
 
 function App() {
  
-
+  const User = "rahul"
+  // {} the inside which is used in Object is an evaluated expression again we cannot use any if or for loop inside an Object
   return (
 
       <>
-        <h1>Heyyy</h1>
+        <h1>Heyyy {User}</h1>
         <Coffee/>
       </>
   
--- 01_project/src/main.jsx
+++ 01_project/src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <h1>Return This</h1>
+  )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children : "Click me to visit Google"
+// } -----> The syntax of the object which the render is expecting is not correct so thats the reason eventhough the code is correct it showing an error while implementing
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+// const anotherUser = "Dravid"
+const reactElementNew = React.createElement(
+  'a',{
+    href:"https://google.com",target:'_blank'
+  },
+  "click me to visit google",
+  // anotherUser as it can be used directly as it is evaluted one again we cannot keep any condition like for or if here as it is an Object {if()} it cannot possible 
+  
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  // MyApp()
+  // reactElement
+  // anotherElement
+  reactElementNew
+  // <App/>
+ 
 )
